432c37faf18a8826dca47a9d4c2236dc3858419e
ganesh216 <ganesh.patidar216@gmail.com>
2022-08-15 20:22:28 +0530

    initial commit to mybookmark data loader

---
+++ src/test/java/com/mybookmark/MybookmarksDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybookmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybookmarksDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
